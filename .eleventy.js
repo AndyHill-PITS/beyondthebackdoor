ac0a2b57a746ca971f73b120a6017401a0d69cde
Andy Hill <andy@practical-it-services.co.uk>
2025-07-10 20:27:33 +0100

    Add Netlify _redirects for CMS password reset handling

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
   eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("_redirects");
 
   return {
     dir: {
